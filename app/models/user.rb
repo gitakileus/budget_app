a2817e7be4f6074001c3430932cc4c797720485e
gitakileus <akileus0902@gmail.com>
2021-12-21 10:47:07 -0600

    Start mailcatcher

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   has_many :groups, dependent: :destroy, through: :groups_payments
   has_many :payments, dependent: :destroy, through: :groups_payments
 
-  validates :name, :email, presence: true
+  # validates :name, :email, presence: true
   validates :password, length: { minimum: 6 }
 end
