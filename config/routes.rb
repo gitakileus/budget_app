bc7ebc0f92dd2f354073fc6759213f240ae66812
gitakileus <akileus0902@gmail.com>
2021-12-21 09:55:31 -0600

    Create and style splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
 
-  resources :users, only: [:index, :show]
+  root 'splash#index'
+
+  resources :groups, only: [:index, :show, :create] do
+    resources :payments, only: [:index, :show, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- test/models/groups_payment_test.rb
+++ test/models/groups_payment_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class GroupsPaymentTest < ActiveSupport::TestCase
   # test "the truth" do
