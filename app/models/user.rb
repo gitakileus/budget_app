a74afc214e002dcc422b0eaa04c8e75676e53931
gitakileus <akileus0902@gmail.com>
2021-12-20 20:00:22 -0600

    Add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :groups, dependent: :destroy, through: :groups_payments
   has_many :payments, dependent: :destroy, through: :groups_payments
+
+  validates :name, :email, presence: true
+  validates :password, length: { minimum: 6 }
 end
