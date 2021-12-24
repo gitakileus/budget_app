61092f9400777747ab9418204a35c582b1d2afcf
gitakileus <akileus0902@gmail.com>
2021-12-24 08:07:24 -0600

    Rid comments from files

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,7 @@ Rails.application.routes.draw do
 
   root 'splash#index'
 
-  # resources :payments, path: 'groups/payments'
-
   resources :groups, only: [:index, :show, :new, :create] do
     resources :payments, only: [:new, :create]
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
